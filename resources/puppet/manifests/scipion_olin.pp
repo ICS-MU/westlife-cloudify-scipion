218fdbe3a669ca54d8d54ffa94079681ca44c8c5
Radim Pesa <pesa@ics.muni.cz>
2017-08-21 15:38:34 +0200

    Chimera package added.

--- resources/puppet/manifests/scipion_olin.pp
+++ resources/puppet/manifests/scipion_olin.pp
@@ -8,7 +8,7 @@ include wget
 include ::westlife::volume
 #include ::archive
 
-$binary_file = 'scipion_v1.0.1_2016-06-30_linux64.tgz'
+$binary_file = 'scipion_v1.0.1_with_chimera.tgz'
 $onedataurl = 'http://get.onedata.org/oneclient.sh'
 ############################################################
 # Install Scipion prerequisities
