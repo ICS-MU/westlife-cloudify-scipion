08ded37480e8772dac71cc4797af847400a83292
Radim Pesa <pesa@ics.muni.cz>
2017-11-02 16:46:43 +0100

    websockify changes

---
+++ resources/puppet/site/websockify/manifests/init.pp
@@ -0,0 +1,36 @@
+class websockify {
+
+vcsrepo { "/opt/novnc/":
+  ensure    => present,
+  provider  => git,
+  source    =>'https://github.com/novnc/noVNC',
+  depth     => '1',
+  before => Service['websockify'],
+}
+exec {'websockify_install':
+  command     => "pip install websockify",
+  path        => '/usr/bin',
+#  environment => ["PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin"],
+  before => Service['websockify'],
+}
+
+
+file {'/etc/systemd/system/websockify.service':
+	ensure => present,
+	source => 'puppet:///modules/websockify/websockify.service',
+  before => Service['websockify'],
+}
+
+#exec {'websockify_enable':
+#  command     => "systemctl enable websockify.service",
+#  path        => '/bin',
+#  before      => Service['websockify'],
+
+#}
+
+service {'websockify':
+  ensure => 'running',
+  enable => 'true',
+}
+
+}
